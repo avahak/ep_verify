3a31313384cabe4b92ea97607ec3446bca4df359
avahak <a.vahakangas.work@gmail.com>
2024-09-07 06:54:15 +0300

    fixed output

--- compare.py
+++ compare.py
@@ -5,18 +5,16 @@ Compare the three different versions of stats.
 import loader
 import results
 
-output_file_name = "output/findings.txt"
-
 def compare_stats(db, table, category, heading, stats, stats_original, stats_tulokset):
     findings = f'--- {heading} ---\n'
-    for id, row in db[table]:
+    for id, row in db[table].items():
         s = stats[category][id]
         so = stats_original[category][id]
         st = stats_tulokset[category][id]
         if s != so:
-            findings = findings + f'{id=}: ' + 'expected: ' + s + ', original: ' + so + '\n'
+            findings = findings + f'{id=}: ' + 'expected: ' + str(s) + ', original: ' + str(so) + '\n'
         if s != st:
-            findings = findings + f'{id=}: ' + 'expected: ' + s + ', tulokset: ' + st + '\n'
+            findings = findings + f'{id=}: ' + 'expected: ' + str(s) + ', tulokset: ' + str(st) + '\n'
     return findings
 
 def output_stats(db):
@@ -27,15 +25,14 @@ def output_stats(db):
     stats_original = results.get_stats_original(db)
     stats_tulokset = results.get_stats_tulokset(db)
 
-    findings = ""
+    game_findings = compare_stats(db, "ep_peli", "game_stats", "game stats", stats, stats_original, stats_tulokset) + "\n\n"
+    match_findings = compare_stats(db, "ep_ottelu", "match_stats", "match stats", stats, stats_original, stats_tulokset) + "\n\n"
+    player_findings = compare_stats(db, "ep_pelaaja", "player_stats", "player stats", stats, stats_original, stats_tulokset) + "\n\n"
+    team_findings = compare_stats(db, "ep_joukkue", "team_stats", "team stats", stats, stats_original, stats_tulokset) + "\n\n"
 
-    findings = findings + compare_stats(db, "ep_peli", "game_stats", "game stats", stats, stats_original, stats_tulokset) + "\n\n"
-    findings = findings + compare_stats(db, "ep_ottelu", "match_stats", "match stats", stats, stats_original, stats_tulokset) + "\n\n"
-    findings = findings + compare_stats(db, "ep_pelaaja", "player_stats", "player stats", stats, stats_original, stats_tulokset) + "\n\n"
-    findings = findings + compare_stats(db, "ep_joukkue", "team_stats", "team stats", stats, stats_original, stats_tulokset) + "\n\n"
-
-    with open(output_file_name, 'w') as f:
-        f.write(findings)
+    for (type, findings) in [("game", game_findings), ("match", match_findings), ("player", player_findings), ("team", team_findings)]:
+        with open(f'output/{type}_findings.txt', 'w') as f:
+            f.write(findings)
 
 
 if __name__ == '__main__':
