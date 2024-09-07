f6a4a0c39db33c8050eb399eff996213ad5f98be
avahak <a.vahakangas.work@gmail.com>
2024-09-07 06:40:40 +0300

    first version done

--- compare.py
+++ compare.py
@@ -5,7 +5,21 @@ Compare the three different versions of stats.
 import loader
 import results
 
-def compare_stats(db):
+output_file_name = "output/findings.txt"
+
+def compare_stats(db, table, category, heading, stats, stats_original, stats_tulokset):
+    findings = f'--- {heading} ---\n'
+    for id, row in db[table]:
+        s = stats[category][id]
+        so = stats_original[category][id]
+        st = stats_tulokset[category][id]
+        if s != so:
+            findings = findings + f'{id=}: ' + 'expected: ' + s + ', original: ' + so + '\n'
+        if s != st:
+            findings = findings + f'{id=}: ' + 'expected: ' + s + ', tulokset: ' + st + '\n'
+    return findings
+
+def output_stats(db):
     """
     Compare the three versions of stats: compute_stats, _tulokset tables, and old results.
     """
@@ -13,22 +27,17 @@ def compare_stats(db):
     stats_original = results.get_stats_original(db)
     stats_tulokset = results.get_stats_tulokset(db)
 
-    # game_stats
-    for id, row in db["ep_peli"]:
-        s = stats["game_stats"][id]
-        so = stats_original["game_stats"][id]
-        st = stats_tulokset["game_stats"][id]
-        if s != so:
-            log
+    findings = ""
 
+    findings = findings + compare_stats(db, "ep_peli", "game_stats", "game stats", stats, stats_original, stats_tulokset) + "\n\n"
+    findings = findings + compare_stats(db, "ep_ottelu", "match_stats", "match stats", stats, stats_original, stats_tulokset) + "\n\n"
+    findings = findings + compare_stats(db, "ep_pelaaja", "player_stats", "player stats", stats, stats_original, stats_tulokset) + "\n\n"
+    findings = findings + compare_stats(db, "ep_joukkue", "team_stats", "team stats", stats, stats_original, stats_tulokset) + "\n\n"
 
+    with open(output_file_name, 'w') as f:
+        f.write(findings)
 
 
 if __name__ == '__main__':
     db = loader.load_db()
-    compare_stats(db)
-
-game_stats = stats["game_stats"]
-match_stats = stats["match_stats"]
-player_stats = stats["player_stats"]
-team_stats = stats["team_stats"]
+    output_stats(db)
